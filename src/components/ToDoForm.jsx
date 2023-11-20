73fe7bdf1b115c3251441e8a76c188e0a541b98a
Kevin Chong <yuen.chong@edu.sait.ca>
2023-11-19 18:18:18 -0700

    lab6

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, ScrollView, Pressable, StyleSheet, TextInput, Button } from 'react-native';
 
 function ToDoForm({ addTask }) {
 
-    const [taskText, setTaskText] = React.useState('');
+    const [taskText, setTaskText] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        // function to get data from tasks.json:src/data/tasks.json
+        const getTasks = () => {
+            try {
+                const data = require('../data/tasks.json');
+                setTasks(data.tasks);
+            } catch (error) {
+                console.error('No data get', error);
+            }
+        }
+        getTasks();
+    }, []);
 
     return (
         <View style={styles.form}>
@@ -14,7 +28,15 @@ function ToDoForm({ addTask }) {
                 value={taskText}
             />
             <Button title="Add Task"
+                style={styles.Button}
                 onPress={() => { addTask(taskText); setTaskText(''); }}
+            />
+            <Button title="Generate Random Task"
+                style={styles.Button}
+                onPress={() => {
+                    const randomTask = tasks[Math.floor(Math.random() * tasks.length)];
+                    setTaskText(randomTask);
+                }}
             />
         </View>
     )
@@ -22,7 +44,8 @@ function ToDoForm({ addTask }) {
 
 const styles = StyleSheet.create({
     form: {
-        flexDirection: 'row',
+        display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'space-between',
         alignItems: 'center',
         marginHorizontal: 20,
@@ -30,12 +53,16 @@ const styles = StyleSheet.create({
     },
     input: {
         flex: 1,
-        borderWidth: 1,
+        borderWidth: 10,
         borderColor: '#ccc',
         paddingHorizontal: 10,
-        paddingVertical: 5,
+        paddingVertical: 2,
         marginRight: 10,
-    },
+        marginBottom: 10,
+        borderRadius: 10,
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
 });
 
 export default ToDoForm;
